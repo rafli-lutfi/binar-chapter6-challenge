216bd3275639305cacd74222bee9551180768d82
rafli-lutfi <rafli.lutfi1818@gmail.com>
2023-05-18 09:45:36 +0700

    add previous challenge

---
+++ src/app.js
@@ -0,0 +1,36 @@
+require("dotenv").config();
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors")
+const router = require("./routes")
+const { PORT } = process.env;
+
+const app = express();
+
+app.use(cors())
+app.use(logger("tiny"));
+app.use(express.json())
+
+app.use("/api/v1", router)
+
+// handle error 404
+app.use((req,res,next) =>{
+  return res.status(404).json({
+    status: false,
+    message: "page not found 404",
+    data: null
+  })
+})
+
+// handle error 500
+app.use((err,req,res,next)=>{
+  return res.status(500).json({
+    status: false,
+    message: err.message,
+    data: null
+  })
+})
+
+app.listen(PORT, () => {
+  console.log(`listening on port:${PORT}`);
+});
---
+++ src/routes/index.js
@@ -0,0 +1,32 @@
+const router = require("express").Router();
+const swaggerUI = require("swagger-ui-express")
+const swaggerDocument = require("../swagger.json")
+
+const user = require("./user")
+
+const component = require("./component")
+const supplier = require("./supplier")
+const componentSupplier = require("./component_supplier")
+
+const product = require("./product")
+const product_component = require("./product_component")
+
+router.use("/docs", swaggerUI.serve)
+router.get("/docs", swaggerUI.setup(swaggerDocument))
+
+router.get("/", (req, res, next) => {
+  res.status(200).json({
+    message: "welcome",
+  });
+});
+
+router.use(user)
+
+router.use("/components", component)
+router.use("/suppliers", supplier)
+router.use("/componentSuppliers", componentSupplier)
+
+router.use("/products", product)
+router.use("/productComponents", product_component)
+
+module.exports = router
